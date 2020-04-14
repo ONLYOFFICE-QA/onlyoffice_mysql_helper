8f1c2f9efdf0b61a1c83c55048ebdc1c828483ba
Pavel Lobashov <shockwavenn@gmail.com>
2020-04-14 16:52:05 +0300

    Remove unused lines

--- lib/onlyoffice_mysql_helper.rb
+++ lib/onlyoffice_mysql_helper.rb
@@ -4,7 +4,3 @@ require 'onlyoffice_logger_helper'
 require 'onlyoffice_mysql_helper/mysql_helper'
 require 'onlyoffice_mysql_helper/mysql_logger'
 require 'onlyoffice_mysql_helper/version'
-
-# Helper class to work with MySQL
-module OnlyofficeMysqlHelper
-end
