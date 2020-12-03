118689f24c0ce93af0e6e29731a165fe2580272c
Pavel Lobashov <shockwavenn@gmail.com>
2020-12-03 14:38:53 +0300

    Add some more missing docs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -23,27 +23,33 @@ RSpec.configure do |config|
   end
 end
 
+# @return [OnlyofficeMysqlHelper::MySQLHelper] default object
 def mysql
   @mysql ||= OnlyofficeMysqlHelper::MySQLHelper.new(database: 'test_by_spec')
 end
 
+# @return [String] table to test drop
 def table_to_drop
   'Tables_in_test_by_spec'
 end
 
+# @return [String] command to test table creation
 def create_table_command
   'id INT PRIMARY KEY AUTO_INCREMENT, '\
   "#{test_column} VARCHAR(25) NOT NULL"
 end
 
+# @return [String] name of test table
 def test_table
   'test'
 end
 
+# @return [String] name of test column
 def test_column
   'test_data'
 end
 
+# @return [Array<Object>] get column records from table
 def get_records(table, column)
   mysql.select_records(table)
        .map { |row| row[column] }
